aab07931c6eae31466d651f1e5d0fcf251ecaf9f
adam.paterson <hello@adampaterson.co.uk>
2024-11-24 08:37:10 +0000

    feat: add blog routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,14 @@ import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.adampaterson.co.uk",
   base: "/",
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), tailwind(), sitemap(), mdx()],
+  content: {
+    sources: ['./src/content']
+  }
 });
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,6 +9,7 @@ const Navigation: React.FC = () => {
   const navItems = [
     { name: "Home", path: "/" },
     { name: "About", path: "/about-me" },
+    { name: "Blog", path: "/blog" },
     { name: "Contact", path: "/contact-me" },
   ];
 
