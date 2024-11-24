8a1a088edc3db866a8effb62ba8dac0378c3ddf0
adam.paterson <hello@adampaterson.co.uk>
2024-11-24 22:28:29 +0000

    feat: add SEO schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,9 +5,11 @@ const blog = defineCollection({
     schema: z.object({
         title: z.string(),
         date: z.string(),
+        description: z.string().optional(),
         excerpt: z.string().optional(),
         tags: z.array(z.string()).optional(),
         image: z.string().optional(),
+        updateDate: z.string().optional(),
     }),
 });
 
